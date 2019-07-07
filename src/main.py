c5ff2400488d8fdfc1175c2512c2cb6d1fd83096
BABA <jaromarg@gmail.com>
2019-07-07 09:40:37 +0200

    code convention fix
    code convention fix

--- src/main.py
+++ src/main.py
@@ -3,35 +3,33 @@ import HookEvent
 import datetime
 import re
 
-
 if __name__ == '__main__':
-    
-    #logging.basicConfig(level=logging.INFO)
-    
-    #create logger with 'spam_application'
+    # logging.basicConfig(level=logging.INFO)
+
+    # create logger with 'spam_application'
     logger = logging.getLogger('application')
     logger.setLevel(logging.INFO)
-    
+
     LogName = str(datetime.datetime.now())
-    logName = re.sub(":","",LogName)+'.log'
+    logName = re.sub(":", "", LogName) + '.log'
     # create file handler which logs even debug messages
-    fhandler = logging.FileHandler(logName)
-    fhandler.setLevel(logging.INFO)
-    
+    f_handler = logging.FileHandler(logName)
+    f_handler.setLevel(logging.INFO)
+
     # create console handler with a higher log level
-    shandler = logging.StreamHandler()
-    shandler.setLevel(logging.INFO)
-    
+    s_handler = logging.StreamHandler()
+    s_handler.setLevel(logging.INFO)
+
     # create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fhandler.setFormatter(formatter)
-    shandler.setFormatter(formatter)
-    
-    #add the handlers to the logger
-    logger.addHandler(fhandler)
-    logger.addHandler(shandler)
-    
+    f_handler.setFormatter(formatter)
+    s_handler.setFormatter(formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(f_handler)
+    logger.addHandler(s_handler)
+
     logger.info('instance main')
-    
+
     hook = HookEvent.HookEvent()
-    hook.hookMouseAndKey()   
+    hook.hook_mouse_and_key()
