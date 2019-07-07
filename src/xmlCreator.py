a488a30f6bc1ed906dfcd44dcca6380ba2c5b55e
BABA <jaromarg@gmail.com>
2019-07-07 10:15:40 +0200

    xml creator
    xml creator

---
+++ src/xmlCreator.py
@@ -0,0 +1,41 @@
+import logging
+import datetime
+import re
+from xml.etree.cElementTree import Element, SubElement, ElementTree
+
+module_logger = logging.getLogger('application.XmlCreator')
+
+
+class XmlCreator(object):
+
+    def __init__(self):
+        self.logger = logging.getLogger('application.XmlCreator')
+        self.logger.debug('creating an instance of XmlCreator')
+        self.root = None
+        self.child = None
+        self.tree = None
+
+    def create_root(self, value):
+        self.root = Element(value)
+
+    def create_child(self, value):
+        self.child = SubElement(self.root, value)
+
+    def create_element(self, field, name, value):
+        SubElement(self.child, field, name=name).text = value
+
+    def compose_tree(self):
+        self.tree = ElementTree(self.root)
+
+    def save_xml(self):
+        filename = str(datetime.datetime.now())
+        filename = re.sub(":", "", filename) + '.xml'
+        self.tree.write(filename)
+
+
+test = XmlCreator()
+test.create_root("root")
+test.create_child("command")
+test.create_element("param", "pos_x", "200")
+test.compose_tree()
+test.save_xml()
